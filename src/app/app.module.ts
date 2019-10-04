9d0703e5b8bd90f376e86af609c6b4bfdc88fc85
Denys Podanenko <d.podanenko@gmail.com>
2019-10-04 18:54:03 +0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,56 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MatListModule} from '@angular/material/list';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+@NgModule({
+   declarations: [
+      AppComponent,
+      MenuComponent,
+      DishdetailComponent,
+      HeaderComponent,
+      FooterComponent,
+      HomeComponent,
+      AboutComponent,
+      ContactComponent,
+   ],
+   imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      MatToolbarModule,
+      FlexLayoutModule,
+      MatListModule,
+      MatGridListModule,
+      MatCardModule,
+      MatButtonModule,
+      AppRoutingModule
+   ],
+   providers: [
+      DishService,
+      PromotionService
+   ],
+   bootstrap: [
+      AppComponent
+   ]
+})
+export class AppModule { }
